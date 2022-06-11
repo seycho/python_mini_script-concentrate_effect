6bb56f96a53f40b075fab04953d6cc5e26022987
Seycho <seycho.han@gmail.com>
2022-06-11 15:24:25 +0900

    fitsr

--- make_effect.py
+++ make_effect.py
@@ -1,44 +1,4 @@
-from numpy import newaxis, arange, uint8, int16, sqrt
-
-def RadialBlur(img, xCenterRatio=0.5, yCenterRatio=0.5, intensity=100, seperNum=10):
-
-    shape = img.shape
-    if len(img.shape) == 3:
-        yShape, xShape, _ = shape
-    else:
-        yShape, xShape = shape
-
-    xRange = arange(xShape) 
-    yRange = arange(yShape)[:,newaxis]
-
-    yCenter = (yShape - 1) * yCenterRatio
-    xCenter = (xShape - 1) * xCenterRatio
-
-    r = sqrt((xRange - xCenter)**2 + (yRange - yCenter)**2)
-    rNormal = r / np.max(r)
-
-    cos = (xRange - xCenter) / r
-    sin = (yRange - yCenter) / r
-
-    intensityArange = arange(0, 1, 1 / (seperNum))
-    intensityArange = (np.sqrt(intensityArange) * intensity).astype(int16)
-
-    rEffect = r[:,:,newaxis] - intensityArange
-    rEffect[rEffect < 0] = 0
-
-    xEffect = (rEffect * cos[:,:,newaxis] + xCenter).astype(int16)
-    yEffect = (rEffect * sin[:,:,newaxis] + yCenter).astype(int16)
-
-    xEffect[xEffect > xShape - 1] = xShape - 1
-    xEffect[xEffect < 0] = 0
-    yEffect[yEffect < 0] = 0
-    yEffect[yEffect > yShape -1] = yShape -1
-
-    step = (rNormal**0.1)[:,:,newaxis] * ((seperNum - np.arange(seperNum)) / seperNum)[::-1]
-
-    imgBlur = ((img[yEffect,xEffect]*step).sum(2) / seperNum).astype(uint8)
-
-    return imgBlur
+from ._utils import RadialBlur
 
 import numpy as np
 import argparse, cv2
